c5829bf166a07a2a0644f4d36e06b956b9a8e68c
RobertoDSouza <101618574+RobertoDSouza@users.noreply.github.com>
2022-05-02 00:17:07 -0300

    Finish
    Sites is done

---
+++ web/js/scroll.js
@@ -0,0 +1,25 @@
+const menuLinks=document.querySelectorAll('.menu a[href^="#"]');
+
+function getDistanceFromTheTop(element){
+const id= element.getAttribute("href");
+return document.querySelector(id).offsetTop;
+}
+
+
+function nativeScroll(distanceFromTheTop){
+    window.scroll({
+        top:distanceFromTheTop,
+        behavior:"smooth",
+    });
+}
+
+function scrollToSection(event){
+    event.preventDefault();
+    const distanceFromTheTop=getDistanceFromTheTop(event.target);
+    nativeScroll(distanceFromTheTop)
+}
+
+menuLinks.forEach((link)=>{
+    link.addEventListener("click",scrollToSection);
+    
+});
